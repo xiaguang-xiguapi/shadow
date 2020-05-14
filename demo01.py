fac5a4c15beccb17f3735264bfa69aa408a23e5c
xiaguang-xiguapi <597951206@qq.com>
2020-05-14 15:58:12 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,36 @@
+"""
+print("hello world!")  #字符串
+print(2333)    #整数
+print(2.333)   #小数
+print(True)    #布尔值
+print(())     #元祖
+print([])     #数组
+print({})     #字典
+
+print(5343)                     
+
+print(2351)
+print(2333) 
+print(2563)
+print("haha")
+print(2<3)
+print(3>2)
+a = "张三"   #把张三这个值赋值给了名字叫a的变量
+print(a)
+"""
+# a = float (input("请输入:"))
+# b = float (input("请输入:"))
+# print("input获取的内容:",a+b)
+#数据格式的转换
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+a = input("a:")
+b = input("b:") 
+
+print("长度:",len(a)+len(b))
